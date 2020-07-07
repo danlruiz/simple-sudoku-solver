492df2d4a0314ac0d9a380f126b5e14bbdbbf64d
Daniel <lacazrud@mcmaster.ca>
2020-07-07 09:16:57 -0400

    adds error handling for invalid inputs

--- app.py
+++ app.py
@@ -7,26 +7,32 @@ app = Flask(__name__)
 @app.route("/", methods=['GET','POST'])
 def solverSudoku():
     
+    error=None
     range_n_rows = range(9)
-
+    
     if request.method == 'POST':
         grid = []
         for row in range(1,10):
             row_content = []
             for column in range(1,10):
+                #Generate html class name and obtain from template
                 html_class = 'row' + str(row) + 'column' + str(column)
                 try:
                     value = int(request.form.get(html_class))
+                #Prevent invalid inputs
                 except ValueError:
                     value = 0
-                if value not in range(0,9):
+                if value not in range(0,10):
                     value = 0
                 row_content.append(value)
             grid.append(row_content)
+        #Check if duplicate values break sudoku rules
+        if solver.isNotDuplicate(puzzle=grid):
+            solved_sudoku = solver.Solve()
+            solved_sudoku.solveSudoku(grid)
+        else:
+            error='Unsolvable Sudoku!'
+        return render_template('sudoku.html', range_n_rows=range_n_rows, grid=grid, error=error)
 
-        solved_sudoku = solver.Solve()
-        solved_sudoku.solveSudoku(grid)
-
-        return render_template('sudoku.html', range_n_rows=range_n_rows, grid=grid)
 
     return render_template('sudoku.html', range_n_rows=range_n_rows)
--- solver.py
+++ solver.py
@@ -1,54 +1,98 @@
 import numpy as np
 
-grid = [[5,3,0,0,7,0,0,0,0],
-        [6,0,0,1,9,5,0,0,0],
-        [0,9,8,0,0,0,0,6,0],
-        [8,0,0,0,6,0,0,0,3],
-        [4,0,0,8,0,3,0,0,1],
-        [7,0,0,0,2,0,0,0,6],
-        [0,0,0,0,0,0,2,8,0],
-        [0,0,0,4,1,9,0,0,5],
-        [0,0,0,0,8,0,0,0,0]]
 
 class Solve():
 
-    def findNextCellToFill(self, grid, i, j):
-            for x in range(i,9):
-                    for y in range(j,9):
-                            if grid[x][y] == 0:
-                                    return x,y
-            for x in range(0,9):
-                    for y in range(0,9):
-                            if grid[x][y] == 0:
-                                    return x,y
-            return -1,-1
+        def findNextCellToFill(self, grid, i, j):
+                for x in range(i,9):
+                        for y in range(j,9):
+                                if grid[x][y] == 0:
+                                        return x,y
+                for x in range(0,9):
+                        for y in range(0,9):
+                                if grid[x][y] == 0:
+                                        return x,y
+                return -1,-1
 
-    def isValid(self, grid, i, j, e):
-            rowOk = all([e != grid[i][x] for x in range(9)])
-            if rowOk:
-                    columnOk = all([e != grid[x][j] for x in range(9)])
-                    if columnOk:
-                            # finding the top left x,y co-ordinates of the section containing the i,j cell
-                            secTopX, secTopY = 3 *(i//3), 3 *(j//3) #floored quotient should be used here. 
-                            for x in range(secTopX, secTopX+3):
-                                    for y in range(secTopY, secTopY+3):
-                                            if grid[x][y] == e:
-                                                    return False
-                            return True
-            return False
+        def isValid(self, grid, i, j, e):
+                rowOk = all([e != grid[i][x] for x in range(9)])
+                if rowOk:
+                        columnOk = all([e != grid[x][j] for x in range(9)])
+                        if columnOk:
+                                # finding the top left x,y co-ordinates of the section containing the i,j cell
+                                secTopX, secTopY = 3 *(i//3), 3 *(j//3) #floored quotient should be used here. 
+                                for x in range(secTopX, secTopX+3):
+                                        for y in range(secTopY, secTopY+3):
+                                                if grid[x][y] == e:
+                                                        return False
+                                return True
+                return False
 
-    def solveSudoku(self,grid, i=0, j=0):
-            i,j = self.findNextCellToFill(grid, i, j)
-            if i == -1:
-                    return True
-            for e in range(1,10):
-                    if self.isValid(grid,i,j,e):
-                            grid[i][j] = e
-                            if self.solveSudoku(grid, i, j):
-                                    return True
-                            # Undo the current cell for backtracking
-                            grid[i][j] = 0
-            return False
+        def solveSudoku(self,grid, i=0, j=0):
+                i,j = self.findNextCellToFill(grid, i, j)
+                if i == -1:
+                        return True
+                for e in range(1,10):
+                        if self.isValid(grid,i,j,e):
+                                grid[i][j] = e
+                                if self.solveSudoku(grid, i, j):
+                                        return True
+                                # Undo the current cell for backtracking
+                                grid[i][j] = 0
+                return False
 
 
 
+def isNotDuplicate(puzzle):
+        #check row
+        for row in puzzle:
+                row_nums = list(range(1,10))
+                for number in row:
+                        if number == 0:
+                                continue
+                        try:
+                                row_nums.remove(number)
+                        except ValueError:
+                                return False
+        #check column
+        for i in range(9):
+                col_nums = list(range(1,10))
+                for row in puzzle:
+                        if row[i] == 0:
+                                continue
+                        try:
+                                col_nums.remove(row[i])
+                        except ValueError:
+                                return False
+        
+        #check sub-grid
+        poss1, poss2, poss3 = list(range(10)), list(range(10)), list(range(10))                
+        for array in range(9):
+                for index in range(3):
+                        a, b, c = index, 3 + index, 6 + index
+                        if (array == 3 or array == 6) and (prev != array):
+                                poss1, poss2, poss3 = list(range(10)), list(range(10)), list(range(10))
+                        prev = array
+                        if checkPoss(puzzle, poss1, array, index) and checkPoss(puzzle, poss2, array, index) and checkPoss(puzzle, poss3, array, index):
+                                pass
+                        else:
+                                return False
+        return True
+
+
+
+def checkPoss( puzzle, poss, array, index):
+        if puzzle[array][index] == 0:
+                return True
+        else:
+                try:
+                        poss.remove(puzzle[array][index])
+                        return True
+                except ValueError:
+                        return False
+
+
+
+        
+                
+                
